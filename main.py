3d62c31038eab562a9bf41bdbfb37a980a29f377
Andrés Court <andres@andrescourt.com>
2022-09-13 15:10:50 -0500

    Make the snake change direction by pressing the arrow keys

--- main.py
+++ main.py
@@ -12,37 +12,16 @@ screen.listen()
 snake = Snake()
 screen.update()
 
-
-# def go_up():
-#     if snake_segments[0].heading() != 270:
-#         snake_segments[0].setheading(90)
-#
-#
-# def go_right():
-#     if snake_segments[0].heading() != 180:
-#         snake_segments[0].setheading(0)
-#
-#
-# def go_left():
-#     if snake_segments[0].heading() != 0:
-#         snake_segments[0].setheading(180)
-#
-#
-# def go_down():
-#     if snake_segments[0].heading() != 90:
-#         snake_segments[0].setheading(270)
-
-
 game_is_on = True
 while game_is_on:
 
-    # screen.onkey(go_up, "Up")
-    # screen.onkey(go_down, "Down")
-    # screen.onkey(go_left, "Left")
-    # screen.onkey(go_right, "Right")
     screen.update()
     time.sleep(0.1)
     snake.move()
+    screen.onkey(snake.go_up, "Up")
+    screen.onkey(snake.go_down, "Down")
+    screen.onkey(snake.go_left, "Left")
+    screen.onkey(snake.go_right, "Right")
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -15,10 +15,25 @@ class Snake:
             self.snake_segments.append(snake)
 
     def move(self):
-        for seg_num in range(len(self.snake_segments) - 1,  0, -1):
-            new_x = self.snake_segments[seg_num-1].xcor()
-            new_y = self.snake_segments[seg_num-1].ycor()
+        for seg_num in range(len(self.snake_segments) - 1, 0, -1):
+            new_x = self.snake_segments[seg_num - 1].xcor()
+            new_y = self.snake_segments[seg_num - 1].ycor()
             self.snake_segments[seg_num].goto((new_x, new_y))
 
         self.snake_segments[0].fd(20)
 
+    def go_up(self):
+        if self.snake_segments[0].heading() != 270:
+            self.snake_segments[0].setheading(90)
+
+    def go_right(self):
+        if self.snake_segments[0].heading() != 180:
+            self.snake_segments[0].setheading(0)
+
+    def go_left(self):
+        if self.snake_segments[0].heading() != 0:
+            self.snake_segments[0].setheading(180)
+
+    def go_down(self):
+        if self.snake_segments[0].heading() != 90:
+            self.snake_segments[0].setheading(270)
