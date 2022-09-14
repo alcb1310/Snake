606259163af46cf339b98954da4a778f7d6f8ad7
Andrés Court <andres@andrescourt.com>
2022-09-14 17:07:36 -0500

    Makes the snake grow each time the snakes eat

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         score.increase_score()
+        snake.grow(snake.tail.position())
 
     # Detect collision with wall
     if snake.head.xcor() >= 290 or snake.head.xcor() <= -290 or snake.head.ycor() <= -290 or snake.head.ycor() >= 270:
